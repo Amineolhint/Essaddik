7e4eb91fd4370845e71f1c385c3c0a85d539b0d0
Amineolhint <oulhintmohamadamine71@gmail.com>
2023-06-07 22:58:05 +0200

    modified

--- main.js
+++ main.js
@@ -56,3 +56,17 @@ function sendEmail(){
     message => alert("sent succesfully")
 );
 }
+
+
+const divContainer = document.querySelector('.brand_list');
+let isClicked = true;
+
+let showOrHide = function(){
+    if(isClicked){
+        divContainer.style.display = 'block';
+        isClicked = false;
+    }else{
+        divContainer.style.display = 'none';
+        isClicked = true;
+    }
+}
